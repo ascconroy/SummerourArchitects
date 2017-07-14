d598aa3d1ab1f6ee2cabb7f4b2eff05d4f6a9544
James Kolsby <jrkolsby@mac.com>
2017-07-13 21:06:36 -0400

    Working menu structure

--- Playground/nav.js
+++ Playground/nav.js
@@ -1,76 +1,60 @@
-console.log("Hey");
+console.log("START");
 
 function Nav(sel, menu) {
-    this.el = document.querySelector(sel);
 
-    // Construct tree of NavItems
+    this.root = new Menu('root', menu); // Stores the root Menu instance
+    this.current = this.root; // Stores the current Menu instance
 
-    this.root; // Stores the root Menu instance
-    this.current; // Stores the current Menu instance
+    this.container = document.querySelector(sel);
+
+	// Create back button
     this.back = document.createElement('div');
+	this.back.setAttribute('class', 'hidden');
+	this.container.append(this.back);
+	
+	this.makeMenu(this.root);
 }
 
-// @param Menu menu
-Nav.prototype.makeMenu(menu) {
-    for (var i = 0; i < menu.length; i++) {
-        var item = menu[i];
-        var name = item.name;
+Nav.prototype.handleEvent = function(e) {
+	this.makeMenu(this.current.mother);
+}
 
-        var link;
-        if (link = item.link)
-            item = new Link(name, link); 
+Nav.prototype.makeMenu = function(menu) {
 
-        var action;
-        if (action = item.action)
-            item = new Action(name, action);
-
-        var menu;
-        if (menu = item.menu)
-            item = new Menu(name, menu);
-    }
-   
     // Remove old elements from DOM tree 
     for (var i = 0; i < this.current.children.length; i++) 
-        this.current.children[i].removeEl();
+        this.current.children[i].el.remove();
 
-    // Set back button to make container menu
-    this.back.addEventListener('click', function() { 
-        this.makeMenu(menu.container);
-    }, false);
+	this.current = menu;
 
-    // Point current to a new menu
-    this.current = menu; 
-    
-    // set back button to makeMenu(menu.container)
-    // append(new NavItem) for each in menu.children (constructor returns DOM Object)
-    // remove each old NavItem.el for each in Nav.current
+	for (var i = 0; i < this.current.children.length; i++) {
+		var item = this.current.children[i];
+		this.container.append(item.el);
+
+		if (item.children) {
+			item.nav = this;
+			item.el.addEventListener('click', item, false);
+		}
+	}
+
+	if (this.current.mother) {
+		this.back.addEventListener('click', this, false);
+		this.back.setAttribute('class', 'visible');
+	} else {
+		this.back.removeEventListener('click', this, false);
+		this.back.setAttribute('class', 'hidden');
+	}
 }
 
 function NavItem(name) {
-    this.el = document.createElement('div');
+	this.tag = 'a';
+    this.el = document.createElement(this.tag);
     this.el.innerHTML = name;
-
-    // Tester
-    this.el.addEventListener("click", function(event) {
-        console.log('clicked!'); 
-    }, false);
+	this.name = name;
 }
 
 // See: https://stackoverflow.com/questions/3785258/how-to-remove-dom-elements-without-memory-leaks
 // DOM keeps all nodes, best practice is to reuse them
-
-NavItem.prototype.getEl = function() {
-    return this.el;
-}
-
-NavItem.prototype.removeEl = function() {
-    this.el.remove();
-}
-
-NavItem.prototype.setEl = function(el) {
-    this.el.remove();
-    this.el = el; 
-}
 
 NavItem.prototype.setIcon = function(src) {
     // remove text and replace with icon
@@ -78,37 +62,60 @@ NavItem.prototype.setIcon = function(src) {
 }
 
 
-// Menu is NavItem
-function Menu(name, menu) {
+Menu.prototype = new NavItem();
+function Menu(name, menu, makeMenu) {
     NavItem.call(this, name);
 
     this.children = []; // Array of NavItem
-    this.container; // Menu
-}
-Menu.prototype = new NavItem();
+	this.mother = null;
 
-// Link is NavItem
+    for (var i = 0; i < menu.length; i++) {
+        var item = menu[i];
+        name = item.name;
+
+        var href;
+        if (href = item.href) {
+            this.children.push(new Link(name, href)); 
+		}
+	
+        var action;
+        if (action = item.action) {
+            this.children.push(new Action(name, action));
+		}
+	
+        var children;
+        if (children = item.children) {
+            item = new Menu(name, children, makeMenu);
+			item.mother = this;
+			this.children.push(item);
+		}
+    }
+}
+
+Menu.prototype.handleEvent = function(e) {
+	this.nav.makeMenu(this);
+}
+
+Link.prototype = new NavItem();
 function Link(name, href) {
     NavItem.call(this, name);
 
     this.el.href = href;
 }
-Link.prototype = new NavItem();
 
-// Action is NavItem
+Action.prototype = new NavItem();
 function Action(name, action) {
     NavItem.call(this, name);
 
     this.el.addEventListener("click", action, false);
 }
-Action.prototype = new NavItem();
 
 
 window.onload = function() {
     var menu = [
         {
             name: 'Collections',
-            menu: [
+            children: [
                 {
                     name: 'Industrial',
                     href: '/'
@@ -125,9 +132,76 @@ window.onload = function() {
         },
         {
             name: 'Test',
-            action: function() {
-                console.log('action!');
-            }
+            children: [
+                {
+                    name: 'One',
+					children: [
+						{
+							name: 'These',
+							href: '/'
+						},
+						{
+							name: 'Are',
+							href: '/'
+						},
+						{
+							name: 'All',
+							href: '/'
+						},
+						{
+							name: 'More Options',
+							children: [
+								{
+									name: 'Third Level',
+									href: '/'
+								},
+								{
+									name: 'Wow so deep',
+									href: '/'
+								},
+								{
+									name: 'Very OOP!',
+									href: '/'
+								}
+							]
+						}
+					]
+                },
+                {
+                    name: 'Two',
+					children: [
+						{
+							name: 'Please',
+							href: '/'
+						},
+						{
+							name: 'Help',
+							href: '/'
+						},
+						{
+							name: 'Me Pleaes',
+							href: '/'
+						}
+					]
+                },
+                {
+                    name: 'Three',
+					children: [
+						{
+							name: 'Wow',
+							href: '/'
+						},
+						{
+							name: 'Huge',
+							href: '/'
+						},
+						{
+							name: 'Menu',
+							href: '/'
+						}
+					]
+                },
+            ]
         },
         {
             name: 'Press',
